ddd4604b19717874f5ae2fab97dd7e6a32ee7f44
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-18 13:40:26 +0100

    set-up email through sendGrid pass in arguments for url and price

--- parser.js
+++ parser.js
@@ -1,19 +1,56 @@
-const nightmare = require('nightmare')();
+require('dotenv').config()
+const sgMail =require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const senderEmail = process.env.SEND_EMAIL
+const receiverEmail = process.env.RECIEVE_EMAIL
+
+const nightmare = require('nightmare')()
+
+const args = process.argv.slice(2)
+const url = args[0]
+const minPrice = args[1]
 
 async function checkPrice() {
-    const priceString = 
-        await nightmare
-        .goto("https://www.amazon.co.uk/Dr-Prepare-Stainless-Steel-BPA-Free-Leak-Proof/dp/B07QMQ7SLV/")
-        .wait("#priceblock_ourprice")
-        .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
-        .end()
-    const priceNumber = parseFloat(priceString.replace('£', '')); 
+    console.log(receiverEmail)
+    console.log(senderEmail)
+    try {
+        const priceString = 
+            await nightmare
+            .goto(url)
+            .wait("#priceblock_ourprice")
+            .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
+            .end()
+        const priceNumber = parseFloat(priceString.replace('£', '')); 
+        console.log(priceNumber)
+       
+        
 
-    if(priceNumber < 10) {
-        console.log("It is cheap")
-    } else {
-        console.log("It is expensive")
+        if(priceNumber < minPrice) {
+            await sendEmail('Price is low',
+            `The price on ${url} has dropped below ${minPrice}`
+            )
+        }
+    } catch (err) {
+        sendEmail('Amazon Price Checker Error',
+            err.message
+            )
+            throw err
+
     }
 }
 
 checkPrice()
+
+function sendEmail(subject, body) {
+    const email = {
+        to: receiverEmail ,
+        from: senderEmail,
+        subject: subject,
+        text: body, 
+        html: body
+    }
+
+    console.log(email);
+
+    return sgMail.send(email)
+}
